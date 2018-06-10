2c709dd094e162b5088aa18873873726ec33551f
Owen Kane <owenkane@Owens-MacBook-Pro.local>
2018-06-10 13:53:21 +0100

    Bird flys to target

--- AR_Duck_Hunt/Assets/Scripts/birdController.cs
+++ AR_Duck_Hunt/Assets/Scripts/birdController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class birdController : MonoBehaviour {
 
+    private Transform targetFocus;
+
 	// Use this for initialization
 	void Start () {
-		
+        targetFocus = GameObject.FindGameObjectWithTag("Target").transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        Vector3 target = targetFocus.position - this.transform.position;
+        Debug.Log(target.magnitude);
+
+        transform.LookAt(targetFocus);
+        float speed = Random.Range(15f, 30f);
+        transform.Translate(0,0, speed * Time.deltaTime);
 	}
 }
